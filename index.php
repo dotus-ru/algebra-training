68b4aec8ba62ea7abcedc0852b8dc1fcf5e910e9
Максим <max960@tut.by>
2016-02-03 12:37:35 +0300

    добавление функции генерации знака + или -

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 function generateRandomNumber($how){
     $count = 1; // инициализация счётчика
     $exit = ''; // переменная для сгенерированного числа
+
     while($count <= $how){
         if($count == 1){ // если генерируем первое число в серии, оно будет от 1 до 9
             $exit .= mt_rand(1,9);
@@ -16,10 +17,20 @@ function generateRandomNumber($how){
         else{ // все последующие генерируются от 0 до 9
             $exit .= mt_rand(0,9); //
         }
+
         $count++;
     }
 
-    return $exit;
+    return $exit; // возвращаем сгенерированное число
 }
 
-print generateRandomNumber(5)." + ".generateRandomNumber(5); // пример вызова генерации числа с заданным кол символов
+function generationAlgebraSymbol(){
+    $letters = "+-"; // удалены спорные буквы
+
+    $exitCode = "";
+    $exitCode .= $letters[mt_rand(0,1)];
+
+    return $exitCode;
+}
+
+print generateRandomNumber(4)." ".generationAlgebraSymbol()." ".generateRandomNumber(4); // пример вызова генерации числа с заданным кол символов
