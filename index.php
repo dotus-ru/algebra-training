f59da52bc9cfd4e851ffcc775a50a221e9c40443
Максим <max960@tut.by>
2016-02-03 11:41:51 +0300

    доработка функции генерации чисел

--- index.php
+++ index.php
@@ -7,13 +7,19 @@
  */
 
 function generateRandomNumber($how){
-    $count;
-    while($count < $how){
-        $exit = mt_rand(0,9);
+    $count = 1; // инициализация счётчика
+    $exit = ''; // переменная для сгенерированного числа
+    while($count <= $how){
+        if($count == 1){ // если генерируем первое число в серии, оно будет от 1 до 9
+            $exit .= mt_rand(1,9);
+        }
+        else{ // все последующие генерируются от 0 до 9
+            $exit .= mt_rand(0,9); //
+        }
         $count++;
     }
 
     return $exit;
 }
 
-print generateRandomNumber(5);
+print generateRandomNumber(5)." + ".generateRandomNumber(5); // пример вызова генерации числа с заданным кол символов
