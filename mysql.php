36c60aead439c0c0228e0dbeef62c136fed2ad2b
Максим <max960@tut.by>
2016-02-05 18:16:24 +0300

    -

--- mysql.php
+++ mysql.php
@@ -1,9 +1,9 @@
 <?php
-if($_SERVER['HTTP_HOST'] == "www.bobruisk.info")
+if($_SERVER['HTTP_HOST'] == "a.bobr.in")
     {
-        $db = mysql_connect('localhost', 'user1164321', 'K9BrmT5U') or die("Сервер баз данных временно недоступен");
+        $db = mysql_connect('localhost', '-', '-') or die("Сервер баз данных временно недоступен");
         mysql_query("SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
-        mysql_select_db('user1164321_bobruisk', $db);
+        mysql_select_db('-', $db);
     }
 
 if($_SERVER['HTTP_HOST'] == "algebra.by")
